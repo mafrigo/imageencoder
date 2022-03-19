fb57dbbcff7fd5df62664e5352b17e8c135db844
Matteo Frigo <matt.frigo@gmail.com>
2022-03-19 19:27:13 +0100

    make tests pytest-friendly

--- test/tests.py
+++ test/tests.py
@@ -4,14 +4,14 @@ from src.image_encoding import *
 from src.encoding import *
 
 
-class Decoding_Test(unittest.TestCase):
+class test_decoding(unittest.TestCase):
     def test_decode_image(self):
         test_image, image_type = load_image('../figures/encoded_github.png')
         decoded_text = decode_image(test_image, image_type=image_type)
         self.assertEqual(decoded_text[0:9], 'Nel mezzo')
 
 
-class Encoding_Test(unittest.TestCase):
+class test_encoding(unittest.TestCase):
     def test_encoding(self):
         encoded_char = get_encoding('A', color_step=0.5, base=6)
         self.assertEqual(encoded_char, [-1., 0.5, 1.])
